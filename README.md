705ffeed1d9f602c9006917dcfd92c205c1f5402
ben0815 <smithbr95@gmail.com>
2016-04-15 15:32:28 -0500

    Project 7 Stage II

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSCE312-VM
+
+# Compile instructions
+# $ make
+
+# Run instructions
+# $ ./vm
+# Enter name of file to translate
--- VM.cpp
+++ VM.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <cctype>
+#include <stdlib.h>
 
 //Global count variable
 //Necessary to make sure if a file contains multiple eq's that each label is
@@ -23,13 +24,92 @@ ParsePush(std::string _line) {
     else
       command = "@" + number + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
   }
-  //@TODO
-  //Different push commands for project 8 will go here (local, this, etc.)
+  else if(_line.find("local") < sz) {
+    std::string number = (_line.substr(9, sz - 9));
+    command = "@LCL\nD=M\n@" + number + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("argument") < sz) {
+    std::string number = (_line.substr(12, sz - 12));
+    command = "@ARG\nD=M\n@" + number + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("this") < sz) {
+    std::string number = (_line.substr(8, sz - 8));
+    command = "@THIS\nD=M\n@" + number + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("that") < sz) {
+    std::string number = (_line.substr(8, sz - 8));
+    command = "@THAT\nD=M\n@" + number + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("temp") < sz) {
+    std::string number = (_line.substr(8, sz - 8));
+    int n = 5 + atoi(number.c_str());
+    std::string n2 = std::to_string(n);
+    command = "@R5\nD=M\n@" + n2 + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("pointer") < sz) {
+    std::string number = (_line.substr(11, sz - 11));
+    if (number == "0")
+      command = "@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+    else if(number == "1")
+      command = "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
+  else if(_line.find("static") < sz) {
+    std::string number = (_line.substr(10, sz - 10));
+    int n = 16 + atoi(number.c_str());
+    std::string n2 = std::to_string(n);
+    command = "@" + n2 + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+  }
 
   return command;
 }
-g
 
+std::string
+ParsePop(std::string _line) {
+  size_t sz = _line.size();
+  std::string command = "";
+
+  //Check type of push command
+  if(_line.find("local") < sz) {
+    //Get number then use it to translate into assembly
+    std::string number = (_line.substr(8, sz - 8));
+    command = "@LCL\nD=M\n@" + number + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("argument") < sz) {
+    std::string number = (_line.substr(11, sz - 11));
+    command = "@ARG\nD=M\n@" + number + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("this") < sz) {
+    std::string number = (_line.substr(7, sz - 7));
+    command = "@THIS\nD=M\n@" + number + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("that") < sz) {
+    std::string number = (_line.substr(7, sz - 7));
+    command = "@THAT\nD=M\n@" + number + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("temp") < sz) {
+    std::string number = (_line.substr(7, sz - 7));
+    int n = 5 + atoi(number.c_str());
+    std::string n2 = std::to_string(n);
+    command = "@R5\nD=M\n@" + n2 + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("pointer") < sz) {
+    std::string number = (_line.substr(10, sz - 10));
+    if (number == "0")
+      command = "@THIS\nD=A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+    else if(number == "1")
+      command = "@THAT\nD=A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else if(_line.find("static") < sz) {
+    std::string number = (_line.substr(9, sz - 9));
+    int n = 16 + atoi(number.c_str());
+    std::string n2 = std::to_string(n);
+    command = "@" + n2 + "\nD=A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+  }
+  else
+    return "";
+
+  return command;
+}
 std::string
 Parse(std::string _line) {
   size_t sz = _line.size();
@@ -38,6 +118,10 @@ Parse(std::string _line) {
   //Check if command is a push constant x command
   if(_line.find("push") < sz) {
     translated = ParsePush(_line);
+    return translated;
+  }
+  else if(_line.find("pop") < sz) {
+    translated = ParsePop(_line);
     return translated;
   }
   else if(_line == "add")
